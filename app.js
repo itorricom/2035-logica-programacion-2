282bc803d8594b9e4a336baa6d84a30022a4593d
Isidoro <itorrico.m@gmail.com>
2024-01-23 00:34:13 -0400

    Evento a un clic y primer funcion

--- app.js
+++ app.js
@@ -4,3 +4,6 @@ titulo.innerHTML = 'Juego del numero secreto';
 let parrafo = document.querySelector('p');
 parrafo.innerHTML = 'Indica un numero de 1 al 10';
 
+function intetoDeUsuario(){
+    alert('Mi función intento');
+}
