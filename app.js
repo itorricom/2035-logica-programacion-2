80aa2f6989f7644bf7cd9a05f2ccad8caf54903f
Isidoro <itorrico.m@gmail.com>
2024-01-23 22:08:06 -0400

    Arreglos Recursividad

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let numeroSecreto = 0;
 let intentos = 0;
+let listaNumerosSorteados = [];
 
 function asignarTextoElemento(elemento, texto){
     let elementoHTML = document.querySelector(elemento);
@@ -51,21 +52,48 @@ function LimpiarCaja(){
 }
 
 function generarNumeroSecreto() {
-    return Math.floor(Math.random()*10)+1;
+    let numeroGenerado = Math.floor(Math.random()*10)+1;
+
+    console.log('numeroGenerado: '+numeroGenerado);
+    console.log('listaNumeroGenerado: '+numeroGenerado);
+    //si el numero generado esta incluido en la lista
+    if(listaNumerosSorteados.includes(numeroGenerado)){
+        return generarNumeroSecreto();
+    }else{
+        listaNumerosSorteados.push(numeroGenerado);
+        return numeroGenerado;
+    }
 }
 
 
+/*
+1.- Crea una función que calcule el índice de masa corporal (IMC) de una persona a partir de su altura en metros y peso en kilogramos, que se recibirán como parámetros.
+*/
+/* function masaCorporal(altura, peso) {
+    return peso/altura;
+}
+console.log(`IMC = ${masaCorporal(1.70,75)}`); */
+
+/*
+  2 Crea una función que calcule el valor del factorial de un número pasado como parámetro.
+*/
+//  n! = n*(n-1)!
+
+/* function factorial(numero) {
+    if(numero == 0 || numero == 1){
+        return 1;
+    }else{
+        return numero * factorial(numero-1);
+    }
+}
+
+console.log("Factorial: "+factorial(5)); */
 
 
-
-
-
-
-
-
-
-
-
+/*  3 Crea una función que convierta un valor en dólares, pasado como parámetro, y devuelva el valor equivalente en reales(moneda brasileña,si deseas puedes hacerlo con el valor del dólar en tu país). Para esto, considera la cotización del dólar igual a R$4,80.
+  4 Crea una función que muestre en pantalla el área y el perímetro de una sala rectangular, utilizando la altura y la anchura que se proporcionarán como parámetros.
+  5 Crea una función que muestre en pantalla el área y el perímetro de una sala circular, utilizando su radio que se proporcionará como parámetro. Considera Pi = 3,14.
+  6 Crea una función que muestre en pantalla la tabla de multiplicar de un número dado como parámetro. */
 
 
 /* 1 Crear una función que muestre "¡Hola, mundo!" en la consola.
